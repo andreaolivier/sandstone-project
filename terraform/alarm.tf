f3bd3a556d939af08a020d32c4892f32f36dd464
ian512549 <ian512549@gmail.com>
2023-10-31 16:28:46 +0000

    some amendments

--- terraform/alarm.tf
+++ terraform/alarm.tf
@@ -1,4 +1,7 @@
 resource "aws_cloudwatch_log_metric_filter" "ingester_error_filtering" {
+#This adds a filter for errors in the logs sent to the log group.
+#The log group should be automatically created by the lambda function, so this will need to be amended once that's done.
+#The task says that this should be saved for major errors, so this should probably be rewritten when we add logging and error reporting to the ingester.
 	name = "IngesterErrorFiltering"
 	pattern = "Error"
 	log_group_name = "log-group:/aws/lambda/LAMBDANAMEHERE"
@@ -11,6 +14,7 @@ resource "aws_cloudwatch_log_metric_filter" "ingester_error_filtering" {
 	}
 
 resource "aws_cloudwatch_metric_alarm" "ingester_alarm" {
+#This triggers an alarm when the ingester function reports an error.
     alarm_name = "CloudwatchAlarm"
     namespace = "IngesterLogging"
     metric_name = "ErrorCount"
@@ -22,12 +26,15 @@ resource "aws_cloudwatch_metric_alarm" "ingester_alarm" {
     alarm_actions = ["arn:aws:sns:eu-west-2:${data.aws_caller_identity.current.account_id}:IngesterLoggingSnsTopic"]
 }
 
-resource "aws_cloudwatch_metric_alarm" "duration_error" {
+resource "aws_cloudwatch_metric_alarm" "ingester_duration_alarm" {
+#I thought it might be a good idea to add a duration error similar to the one from the sprint.
+#If I understand this right it should raise an error when the ingester runs for more than 10 minutes.
+#This shouldn't need a filter since it uses the AWS/Lambda namespace, which lambda functions automatically send metrics to.
     alarm_name = "DurationAlarm"
     comparison_operator = "GreaterThanOrEqualToThreshold"
     evaluation_periods = 1
     namespace = "AWS/Lambda"
-    threshold = 1200
+    threshold = 600
     metric_name = "Duration"
     statistic = "Maximum"
     period = 60
