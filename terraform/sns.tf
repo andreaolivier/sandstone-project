4665cb7bb9dee8187d92b8755cf43fccd2f45db7
ian512549 <ian512549@gmail.com>
2023-10-31 14:54:38 +0000

    Fixing some errors

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -1,8 +1,8 @@
 variable "amazon_account_id" {}
-
+#This is the variable called for in the subscription resource below. When this file is run with terraform plan, it should ask you to input your amazon account id, which is a 12-digit number.
 resource "aws_sns_topic" "ingester_logging_sns" {
     #This simply creates the SNS topic, which presently does nothing.
-	name = "IngesterLoggingSnsTopic"
+	name = "ingester-logging-sns-topic"
 	}
 
 
@@ -10,7 +10,7 @@ resource "aws_sns_topic" "ingester_logging_sns" {
 resource "aws_sns_topic_subscription" "ingester_subscription" {
     #This creates our group email's subscription to the SNS topic. This will send an email that will activate the subscription once we respond.
 	protocol = "email"
-    topic_arn = "arn:aws:sns:eu-west-2:${var.amazon_account_id}:ingester_logging_sns_topic"
+    topic_arn = "arn:aws:sns:eu-west-2:${var.amazon_account_id}:ingester-logging-sns-topic"
 	endpoint = "sandstone.de@gmail.com"
 	}
 
