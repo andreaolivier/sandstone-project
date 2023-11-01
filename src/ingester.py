039e3573c7dfd4d0a402749ef538aee36f26abed
andreaolivier <andrea.olivier2004@gmail.com>
2023-11-01 14:00:37 +0000

    adds ingester functions with tests

---
+++ src/ingester.py
@@ -0,0 +1,123 @@
+from datetime import datetime as dt
+import json
+import os
+from pg8000.native import Connection
+from dotenv import load_dotenv
+import boto3
+from src.utils.ingestion import get_all_table_data
+
+
+def get_connection():
+    """Establishes a connection to a database and returns the connection
+    object.
+        Returns:
+            column_names (list): All column names from table_name.
+    """
+    return Connection(
+        user=os.environ['DB_USER'],
+        database=os.environ['DB_NAME'],
+        port=os.environ['DB_PORT'],
+        host=os.environ['DB_HOST'],
+        password=os.environ['DB_PASSWORD']
+    )
+
+
+def get_object_list(s3, bucket_name):
+    """Returns all the objects in a bucket from the passed bucket_name.
+        Parameters:
+            s3 (botocore.client.S3): An S3 client.
+            bucket_name (str): The name of the S3 bucket.
+        Returns:
+            bucket_object_list (list): A list of bucket objects.
+    """
+    response = s3.list_objects_v2(Bucket=bucket_name)
+
+    if 'Contents' in response:
+        file_names = [bucket_obj['Key']
+                      for bucket_obj in response['Contents']]
+
+        return file_names
+    return []
+
+
+def get_last_ids(s3, bucket_name):
+    """Returns the last_ids dictionary from an existing S3 Object
+        Parameters:
+            s3 (botocore.client.S3): An S3 client.
+            bucket_name (str): The name of the S3 bucket.
+        Returns:
+            last_ids (dict): The primary key id number for each table from the
+            previous ingestion.
+    """
+    bucket_objects = get_object_list(s3, bucket_name)
+
+    if len(bucket_objects) == 0:
+        return {}
+
+    latest_file = sorted(bucket_objects)[-1]
+
+    s3_response = s3.get_object(
+        Bucket=bucket_name,
+        Key=latest_file
+    )
+
+    file_content = s3_response.get('Body').read()
+    json_content = json.loads(file_content)
+
+    return json_content['last_ids']
+
+
+def check_for_new_values(data):
+    """Returns a boolean if the table data passed contains any new values in
+    the table data meaning there is data to upload.
+            data (dict): The big table data dictionary.
+        Returns:
+            new_values (bool): If there are values in the dictionary.
+    """
+    for index, data_values in enumerate(list(data.values())):
+        if index == 0:
+            continue
+        for value in data_values.values():
+            if value != []:
+                return True
+    return False
+
+
+def ingestion_handler():
+    """Handles the ingestion process. Checks for existing S3 Object if there
+    isn't one downloads everything from the database and saves it in a file
+    in the format of 'year-month-day/hour-minute.json'. If there is an existing
+    file it extracts the last_id and only uploads the object if there are new
+    changes from the database.
+    """
+    try:
+        s3 = boto3.client('s3')
+        conn = get_connection()
+
+        bucket_name = 'tester-bucket-sandstone'
+
+        last_ids = get_last_ids(s3, bucket_name)
+
+        data = get_all_table_data(conn, last_ids)
+
+        if check_for_new_values(data):
+            print('saving')
+            json_str = json.dumps(data, default=str)
+
+            date = dt.today().strftime('%y-%m-%d')
+            hour = dt.today().strftime('%H-%M')
+
+            print(hour, date)
+            s3.put_object(
+                Bucket=bucket_name,
+                Key=f"{date}/{hour}.json",
+                Body=json_str,
+            )
+    except s3.exceptions.NoSuchKey as NoKeyError:
+        print(NoKeyError)
+    except Exception as e:
+        print(e)
+
+
+load_dotenv()
+ingestion_handler()
---
+++ test/test_ingester_functions/test_ingestion_handler.py
@@ -0,0 +1,269 @@
+"""Tests for ingestion_handler function"""
+import os
+import json
+import pytest
+from unittest.mock import patch
+from moto import mock_s3
+import boto3
+import botocore
+from datetime import datetime
+from src.ingester import ingestion_handler
+
+
+@pytest.fixture
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+
+@pytest.fixture
+def s3(aws_credentials):
+    with mock_s3:
+        yield boto3.client("s3", 'eu-west-2')
+
+
+@mock_s3
+def test_ingestion_handler_creates_object_if_bucket_empty_with_data():
+    """Test that ingestion_handler() creates a new S3 Object with database
+    data in bucket when the bucket is empty.
+    """
+    data = {
+        "last_ids": {
+            "currency": 0,
+            "payment": 0,
+            "department": 0,
+            "design": 0,
+            "counterparty": 0,
+            "purchase_order": 0,
+            "payment_type": 0,
+            "sales_order": 0,
+            "address": 0,
+            "staff": 0,
+            "transaction": 0
+        },
+        "currency": {
+            "currency_id": [],
+            "currency_code": [],
+            "created_at": [],
+            "last_updated": []
+        },
+        "design": {
+            "design_id": [],
+            "created_at": [],
+            "design_name": ['Vale'],
+            "file_location": [],
+            "file_name": [],
+            "last_updated": [123123, 32423, 321]
+        },
+        "payment_type": {
+            "payment_type_id": [],
+            "payment_type_name": [],
+            "created_at": ['MORE VALUES'],
+            "last_updated": []
+        }
+    }
+
+    client = botocore.session.get_session().create_client('s3')
+    client.create_bucket(
+        Bucket='tester-bucket-sandstone',
+        CreateBucketConfiguration={'LocationConstraint': 'eu-west-2'}
+    )
+
+    with patch('src.ingester.get_all_table_data') as get_table:
+        with patch('src.ingester.dt') as dt:
+            dt.today.return_value = datetime(
+                2023, 11, 1, 12, 29, 7, 908653)
+
+            get_table.return_value = data
+            ingestion_handler()
+
+    s3_response = client.get_object(
+        Bucket='tester-bucket-sandstone',
+        Key='23-11-01/12-29.json'
+    )
+
+    file_content = s3_response.get('Body').read()
+    json_content = json.loads(file_content)
+
+    assert data == json_content
+
+
+@mock_s3
+def test_creates_s3_object_if_bucket_is_empty_with_database_data():
+    """Test that ingestion_handler() doesn't create new object when there is
+    no new data.
+    """
+    data = {
+        "last_ids": {
+            "currency": 0,
+            "payment": 0,
+            "department": 0,
+            "design": 0,
+            "counterparty": 0,
+            "purchase_order": 0,
+            "payment_type": 0,
+            "sales_order": 0,
+            "address": 0,
+            "staff": 0,
+            "transaction": 0
+        },
+        "currency": {
+            "currency_id": [],
+            "currency_code": [],
+            "created_at": [],
+            "last_updated": []
+        },
+        "design": {
+            "design_id": [],
+            "created_at": [],
+            "design_name": [],
+            "file_location": [],
+            "file_name": [],
+            "last_updated": []
+        },
+        "payment_type": {
+            "payment_type_id": [],
+            "payment_type_name": [],
+            "created_at": [],
+            "last_updated": []
+        }
+    }
+
+    client = botocore.session.get_session().create_client('s3')
+    client.create_bucket(
+        Bucket='tester-bucket-sandstone',
+        CreateBucketConfiguration={'LocationConstraint': 'eu-west-2'}
+    )
+
+    with patch('src.ingester.get_all_table_data') as get_table:
+        with patch('src.ingester.dt') as dt:
+            dt.today.return_value = datetime(
+                2023, 11, 1, 12, 29, 7, 908653)
+
+            get_table.return_value = data
+            ingestion_handler()
+    try:
+        client.get_object(
+            Bucket='tester-bucket-sandstone',
+            Key='23-11-01/12-29.json'
+        )
+        assert False
+    except client.exceptions.NoSuchKey:
+        assert True
+
+
+@mock_s3
+def test_creates_s3_object_if_bucket_has_objects_with_database_data_with_obj():
+    """Test that ingestion_handler() creates new object when there is new data
+    and there is existing object with data.
+    """
+    data = {
+        "last_ids": {
+            "currency": 0,
+            "payment": 0,
+            "department": 0,
+            "design": 0,
+            "counterparty": 0,
+            "purchase_order": 0,
+            "payment_type": 0,
+            "sales_order": 0,
+            "address": 0,
+            "staff": 0,
+            "transaction": 0
+        },
+        "currency": {
+            "currency_id": [1, 2, 3],
+            "currency_code": [0, 0, 0],
+            "created_at": [0, 0, 0],
+            "last_updated": [0, 0, 0]
+        },
+        "design": {
+            "design_id": [1, 2],
+            "created_at": [],
+            "design_name": [],
+            "file_location": [],
+            "file_name": [],
+            "last_updated": []
+        },
+        "payment_type": {
+            "payment_type_id": [1, 2],
+            "payment_type_name": [],
+            "created_at": [],
+            "last_updated": []
+        }
+    }
+    data2 = {
+        "last_ids": {
+            "currency": 0,
+            "payment": 0,
+            "department": 0,
+            "design": 0,
+            "counterparty": 0,
+            "purchase_order": 0,
+            "payment_type": 0,
+            "sales_order": 0,
+            "address": 0,
+            "staff": 0,
+            "transaction": 0
+        },
+        "currency": {
+            "currency_id": [],
+            "currency_code": [],
+            "created_at": [],
+            "last_updated": []
+        },
+        "design": {
+            "design_id": [],
+            "created_at": [],
+            "design_name": [],
+            "file_location": [],
+            "file_name": [],
+            "last_updated": []
+        },
+        "payment_type": {
+            "payment_type_id": [3, 4],
+            "payment_type_name": [],
+            "created_at": [],
+            "last_updated": []
+        }
+    }
+
+    client = botocore.session.get_session().create_client('s3')
+    client.create_bucket(
+        Bucket='tester-bucket-sandstone',
+        CreateBucketConfiguration={'LocationConstraint': 'eu-west-2'}
+    )
+
+    with patch('src.ingester.get_all_table_data') as get_table:
+        with patch('src.ingester.dt') as dt:
+            dt.today.return_value = datetime(
+                2023, 11, 1, 12, 29, 7, 908653)
+            get_table.return_value = data
+
+            ingestion_handler()
+
+            dt.today.return_value = datetime(
+                2023, 11, 1, 12, 35, 7, 908653)
+            get_table.return_value = data2
+
+            ingestion_handler()
+
+    response = client.list_objects_v2(Bucket='tester-bucket-sandstone')
+    file_names = [bucket_obj['Key']
+                  for bucket_obj in response['Contents']]
+
+    assert file_names == ['23-11-01/12-29.json', '23-11-01/12-35.json']
+
+    s3_response = client.get_object(
+        Bucket='tester-bucket-sandstone',
+        Key='23-11-01/12-35.json'
+    )
+
+    file_content = s3_response.get('Body').read()
+    json_content = json.loads(file_content)
+
+    assert json_content == data2
