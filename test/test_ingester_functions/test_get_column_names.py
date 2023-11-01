0e41fe0ef70f199eca40eb747003ee4ff46c9a76
andreaolivier <andrea.olivier2004@gmail.com>
2023-11-01 15:42:08 +0000

    replaces get_connection for pg8000 connection in test file

--- test/test_ingester_functions/test_get_column_names.py
+++ test/test_ingester_functions/test_get_column_names.py
@@ -1,13 +1,23 @@
 """Tests for check_for_new_values function"""
 from src.utils.ingestion import get_column_names
-from src.ingester import get_connection
+from pg8000.native import Connection
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def test_get_column_names_returns_a_list_of_all_column_names():
     """Test that get_column_names() returns a list of columns names of
     table_name passed .
     """
-    conn = get_connection()
+    conn = Connection(
+        user=os.environ['DB_USER'],
+        database=os.environ['DB_NAME'],
+        port=os.environ['DB_PORT'],
+        host=os.environ['DB_HOST'],
+        password=os.environ['DB_PASSWORD']
+    )
     result = get_column_names(conn, 'currency')
     result2 = get_column_names(conn, 'payment_type')
 
