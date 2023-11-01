f5fb2130741db9256ba0dc9ef9ed4353f5eb0666
pablobgldo <pablobgldo@gmail.com>
2023-11-01 16:01:37 +0000

    Added test file for get_all_table_data

---
+++ test/test_ingester_functions/test_get_all_table_data.py
@@ -0,0 +1,17 @@
+from src.utils.ingestion import get_all_table_data
+from pg8000.native import Connection
+import os
+
+
+def test_returns_a_dictionary_of_dictionaries():
+    pass
+    # conn = Connection(
+    #     user=os.environ['DB_USER'],
+    #     database=os.environ['DB_NAME'],
+    #     port=os.environ['DB_PORT'],
+    #     host=os.environ['DB_HOST'],
+    #     password=os.environ['DB_PASSWORD']
+    # )
+    
+    # assert type(get_all_table_data(conn)) == dict
+    
