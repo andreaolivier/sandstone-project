83a04f8366b1e85f51fc187546d37075e040f01d
ian512549 <ian512549@gmail.com>
2023-11-02 15:28:49 +0000

    adding env secrets to lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,6 +4,15 @@ resource "aws_lambda_function" "ingester_lambda" {
     role = aws_iam_role.ingester_role.arn
     runtime = "python3.11"
     handler = "${var.python_file_name}.ingestion_handler"
+    environment {
+      variables = {
+        DB_USER = "${ secrets.DB_USER }",
+        DB_NAME = "${ secrets.DB_NAME }",
+        DB_PORT = "${ secrets.DB_PORT }",
+        DB_HOST = "${ secrets.DB_HOST }",
+        DB_PASSWORD = "${ secrets.DB_PASSWORD }"
+      }
+    }
 }
 
 resource "aws_lambda_permission" "allow_eventbridge" {
