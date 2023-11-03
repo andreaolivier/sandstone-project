776b500de1e1e6fdf05852eb3d7392b8dc4188ab
pablobgldo <pablobgldo@gmail.com>
2023-11-03 10:15:14 +0000

    deleted dependencies bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,10 +2,6 @@ resource "aws_s3_bucket" "ingested_data_bucket" {
   bucket = "sandstone-ingested-data"
 }
 
-resource "aws_s3_bucket" "dependencies_bucket" {
-  bucket = "sandstone-dependencies"
-}
-
 resource "aws_s3_bucket" "processed_data_bucket" {
   bucket = "sandstone-processed-data"
 }
