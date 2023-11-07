8a211a5099d122e41a5166e28a1ffcee2e95c7fe
pablobgldo <pablobgldo@gmail.com>
2023-11-07 10:09:07 +0000

    final changes to python zip file

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,10 +1,10 @@
 resource "aws_lambda_function" "ingester_lambda" {
     # filename = "${path.module}/../${var.lambda_name}.zip"
-    filename = "${path.module}/../python1.zip"
+    filename = "${path.module}/../python.zip"
     function_name = "${var.lambda_name}"
     role = aws_iam_role.ingester_role.arn
     runtime = "python3.11"
-    handler = "python1.ingestion_handler"
+    handler = "python.ingestion_handler"
     # handler = "${var.python_file_name}.ingestion_handler"
     layers = [aws_lambda_layer_version.lambda_layer.arn]
     timeout = 60
--- terraform/layer.tf
+++ terraform/layer.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_layer_version" "lambda_layer" {
-    filename   = "${path.module}/../python1.zip"
+    filename   = "${path.module}/../python.zip"
     layer_name = "pandas_module_layer"
     compatible_runtimes = ["python3.11"]
 }
