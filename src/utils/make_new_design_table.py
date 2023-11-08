0352d5fc4cbf6b8046b9246dd9c51d998eab8b8e
ian512549 <ian512549@gmail.com>
2023-11-08 09:11:56 +0000

    last adjustments

--- src/utils/make_new_design_table.py
+++ src/utils/make_new_design_table.py
@@ -4,8 +4,6 @@ def make_new_design_table(big_dict):
     '''This takes the full data dictionary and returns a dictionary containing lists of the appropriate columns, maintaining the same structure otherwise.'''
     design_table = big_dict['design']
     
-    if design_table['design_id'] == []:
-        return {}
 
     design_columns = ['design_id', 'design_name', 'file_location', 'file_name']
 
--- test/test_processing_functions/test_new_design_table.py
+++ test/test_processing_functions/test_new_design_table.py
@@ -47,11 +47,13 @@ def sample_data_empty_table():
     }
     return sample_data
 
+
 def test_design_table_returns_dict_of_lists(sample_data_table):
     output = make_new_design_table(sample_data_table)
     assert isinstance(output, dict)
     for i in output:
         assert isinstance(output[i], list)
+
 
 def test_design_table_returns_correct_dict(sample_data_table):
     output = make_new_design_table(sample_data_table)
@@ -65,6 +67,29 @@ def test_design_table_returns_correct_dict(sample_data_table):
             "granite-20220430-l5fs.json"]
     }
 
-def test_design_table_returns_empty_dict_when_no_data(sample_data_empty_table):
-    output = make_new_design_table(sample_data_empty_table)
-    assert output == {}
+def test_design_does_not_mutate_input(sample_data_table):
+    input = sample_data_table
+    make_new_design_table(input)
+    assert input == {"design": {
+        "design_id": [7, 8, 5, 4],
+        "created_at": ["2022-11-03 14:20:49.962000",
+            "2022-11-03 14:20:49.962000",
+            "2022-11-03 14:20:49.962000",
+            "2022-11-03 14:20:49.962000"
+            ],
+        "design_name": ["Wooden", "Wooden", "Granite", "Granite"],
+        "file_location": ["/usr/obj", "/usr", "/Network", "/usr/local/src"],
+        "file_name": ["wooden-20211114-otpq.json",
+            "wooden-20220717-npgz.json",
+            "granite-20220419-b6n4.json",
+            "granite-20220430-l5fs.json"],
+        "last_updated": ["2022-11-03 14:20:49.962000",
+            "2022-11-03 14:20:49.962000",
+            "2022-11-03 14:20:49.962000",
+            "2022-11-03 14:20:49.962000"]
+    },
+    'payment_type': {
+        'payment_type_id': [],
+        'payment_type_name': []
+    }
+    }
