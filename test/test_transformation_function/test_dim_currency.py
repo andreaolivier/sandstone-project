4abfdbc0f3d1a148527c4c88a402461b419c056c
George Baldwin <georgebaldwin@Georges-MacBook-Air.local>
2023-11-08 10:57:54 +0000

    refactored get_currency_data to include currency_name data, and relevant tests

--- test/test_transformation_function/test_dim_currency.py
+++ test/test_transformation_function/test_dim_currency.py
@@ -1,4 +1,4 @@
-from src.dim_currency import get_currency_data
+from src.utils.dim_currency import get_currency_data
 
 TEST_INPUT_DATA = {"currency": {
     "currency_id": [1, 2, 3],
@@ -16,11 +16,12 @@ TEST_INPUT_DATA = {"currency": {
   }}
 
 def test_dict_has_correct_keys():
-    assert list(get_currency_data(TEST_INPUT_DATA).keys()) == ["currency_id", "currency_code"]
+    assert list(get_currency_data(TEST_INPUT_DATA).keys()) == ["currency_id", "currency_code", "currency_name"]
 
 def test_dict_has_3_id_and_3_code():
     assert len(get_currency_data(TEST_INPUT_DATA)['currency_id']) is 3
     assert len(get_currency_data(TEST_INPUT_DATA)['currency_code']) is 3
+    assert len(get_currency_data(TEST_INPUT_DATA)['currency_name']) is 3
 
 def test_get_currency_data_returns_dict():
     assert isinstance(get_currency_data(TEST_INPUT_DATA), dict)
