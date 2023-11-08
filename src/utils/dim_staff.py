1d543697c34bf8868ed876f582d61ae4bdc1b365
john <mustchjo@tutanota.com>
2023-11-08 15:40:44 +0000

    get full dept table every time staff is updated

--- src/utils/dim_staff.py
+++ src/utils/dim_staff.py
@@ -1,11 +1,25 @@
+from pg8000.native import Connection
+from src.ingestion import get_table_data
+import os
+
 def create_dim_staff(dict):
     try:
+        conn = Connection(
+            user=os.environ['DB_USER'],
+            database=os.environ['DB_NAME'],
+            port=os.environ['DB_PORT'],
+            host=os.environ['DB_HOST'],
+            password=os.environ['DB_PASSWORD']
+        )
+        dept_data = get_table_data(conn, 'department')
         dim_staff = {
             'staff_id': dict['staff']['staff_id'],
             'first_name': dict['staff']['first_name'],
             'last_name': dict['staff']['last_name'],
-            'department_name': [dict['department']['department_name'][id - 1] for id in dict['staff']['department_id']],
-            'location': [dict['department']['location'][id - 1] for id in dict['staff']['department_id']],
+            'department_name': [dept_data['department_name'][id - 1] 
+                                for id in dict['staff']['department_id']],
+            'location': [dept_data['location'][id - 1] 
+                         for id in dict['staff']['department_id']],
             'email_address': dict['staff']['email_address']
         }
         return (dim_staff)
