a0f9588a6b4dd7f638342e5d915ca490e94f94cf
pablobgldo <pablobgldo@gmail.com>
2023-11-09 16:19:55 +0000

    removed duplicates

--- terraform/processed_data.tf
+++ terraform/processed_data.tf
@@ -1,7 +1,3 @@
-data "aws_caller_identity" "current" {}
-
-data "aws_region" "current" {}
-
 data "archive_file" "process_lambda" {
     type = "zip"
     source_file = "${path.module}/../src/processing_handler.py"
