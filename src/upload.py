e687192b787774142d15e135af9c2267330d0e64
andreaolivier <andrea.olivier2004@gmail.com>
2023-11-09 12:20:50 +0000

    upload lambda finished.

--- src/upload.py
+++ src/upload.py
@@ -1,67 +1,67 @@
 import pg8000
 import pandas as pd
 import awswrangler as wr
-from pg8000.native import Connection, identifier, literal
 import pg8000.dbapi
+import logging
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger('MyLogger')
+logger.setLevel(logging.INFO)
 
 
-# def get_connection():
-#     """Establishes a connection to a database and returns the connection
-#     object.
-#         Returns:
-#             column_names (list): All column names from table_name.
-#     """
-#     return Connection(
-#         user='john',
-#         database='john',
-#         port=5432,
-#         host='localhost',
-#         password='sqlpass'
-#     )
+def lambda_handler(event, context):
+    try:
+        conn = pg8000.dbapi.connect(
+            user='andrea',
+            database='test_processing',
+            port=5432,
+            host='localhost',
+            password='password123')
+        cursor = conn.cursor()
 
-conn = pg8000.dbapi.connect(        
-        user='john',
-        database='john',
-        port=5432,
-        host='localhost',
-        password='sqlpass')
-cursor = conn.cursor()
+        s3_bucket_name = event['Records'][0]['s3']['bucket']['name']
+        s3_object_name = event['Records'][0]['s3']['object']['key']
 
+        logger.info('Bucket is %s', s3_bucket_name)
+        logger.info('Object key is %s', s3_object_name)
 
-def upload_handler():
-    # s3_bucket_name = event['Records'][0]['s3']['bucket']['name']
-    # s3_object_name = event['Records'][0]['s3']['object']['key']
-    s3_object_name = '23-11-01/10-12/test.parquet'
+        if s3_object_name[-7] != 'parquet':
+            raise InvalidFileTypeError
 
-    # if s3_object_name[-7] != 'parquet':
-    #     raise InvalidFileTypeError
+        df = wr.s3.read_parquet(
+            path=[f's3://{s3_bucket_name}/{s3_object_name}'])
 
-    df = wr.s3.read_parquet(
-        path=[f's3://tester-bucket-sandstone/{s3_object_name}'])
-    table_dict = df.to_dict('tight')
-    table_name = s3_object_name[15:-8]
-    columns = [str(f'"{val}"') for val in table_dict['columns']]
+        table_dict = df.to_dict('tight')
 
-    placeholders = ', '.join(['%s'] * table_dict['data'][0])
-    for row in table_dict['data']:
-        # values = [str(f'"{val}"') for val in row if ]
-        # print(f'''insert into test values ({', '.join(values)}); ''')  
-        # query_str = (
-        #     'INSERT INTO test '
-        #     '(department_id, department_name) '
-        #     'VALUES (:id, :dept); '
-        # )
-        # get_connection().run(query_str, id=1, dept='test')
+        table_name = s3_object_name[15:-8]
 
+        placeholders = ', '.join(['%s'] * len(table_dict['data'][0]))
 
-        cursor.execute(
-            "INSERT INTO test (department_id, department_name) "
-            f" VALUES ({placeholders}) ",
-            tuple(f'{str(i)}' for i in row)
-        )   
+        for row in table_dict['data']:
+            cursor.execute(
+                f"INSERT INTO {table_name} "
+                f" VALUES ({placeholders}) ",
+                tuple(f'{str(i)}' for i in row)
+            )
+
         conn.commit()
-
-upload_handler()
+        logger.info('upload success')
+    except KeyError as k:
+        logger.error('Error retrieving data %s', k)
+    except ClientError as c:
+        if c.response['Error']['Code'] == 'NoSuchKey':
+            logger.error(f'No object found - {s3_object_name}')
+        elif c.response['Error']['Code'] == 'NoSuchBucket':
+            logger.error(f'No such bucket - {s3_bucket_name}')
+        else:
+            raise
+    except UnicodeError:
+        logger.error(f'File {s3_object_name} is not a valid text file')
+    except InvalidFileTypeError:
+        logger.error(f'File {s3_object_name} is not a valid text file')
+    except Exception as e:
+        logger.error(e)
+        raise RuntimeError
 
 
 class InvalidFileTypeError(Exception):
