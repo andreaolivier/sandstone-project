d59e491f3f7af646ce7cedf3c9e6fcc9181d105a
john <mustchjo@tutanota.com>
2023-11-09 11:45:39 +0000

    changed to use dbapi and cursor

--- src/upload.py
+++ src/upload.py
@@ -2,21 +2,30 @@ import pg8000
 import pandas as pd
 import awswrangler as wr
 from pg8000.native import Connection, identifier, literal
+import pg8000.dbapi
 
 
-def get_connection():
-    """Establishes a connection to a database and returns the connection
-    object.
-        Returns:
-            column_names (list): All column names from table_name.
-    """
-    return Connection(
-        user='andrea',
-        database='test_processing',
+# def get_connection():
+#     """Establishes a connection to a database and returns the connection
+#     object.
+#         Returns:
+#             column_names (list): All column names from table_name.
+#     """
+#     return Connection(
+#         user='john',
+#         database='john',
+#         port=5432,
+#         host='localhost',
+#         password='sqlpass'
+#     )
+
+conn = pg8000.dbapi.connect(        
+        user='john',
+        database='john',
         port=5432,
         host='localhost',
-        password='password123'
-    )
+        password='sqlpass')
+cursor = conn.cursor()
 
 
 def upload_handler():
@@ -33,12 +42,24 @@ def upload_handler():
     table_name = s3_object_name[15:-8]
     columns = [str(f'"{val}"') for val in table_dict['columns']]
 
+    placeholders = ', '.join(['%s'] * table_dict['data'][0])
     for row in table_dict['data']:
         # values = [str(f'"{val}"') for val in row if ]
         # print(f'''insert into test values ({', '.join(values)}); ''')  
+        # query_str = (
+        #     'INSERT INTO test '
+        #     '(department_id, department_name) '
+        #     'VALUES (:id, :dept); '
+        # )
+        # get_connection().run(query_str, id=1, dept='test')
 
-        get_connection().run('insert into test (department_id, department_name, location, manager, created_at, last_updated) values (1, "Sales", "Manchester", "Richard Roma", "2022-11-03 14:20:49.962000", "2022-11-03 14:20:49.962000") ')
 
+        cursor.execute(
+            "INSERT INTO test (department_id, department_name) "
+            f" VALUES ({placeholders}) ",
+            tuple(f'{str(i)}' for i in row)
+        )   
+        conn.commit()
 
 upload_handler()
 
