911898c7ee8bfd68713f7615379f1d12b2c0cdcd
pablobgldo <pablobgldo@gmail.com>
2023-11-09 10:35:41 +0000

    added s3 permission

--- terraform/process_lambda.tf
+++ terraform/process_lambda.tf
@@ -7,3 +7,11 @@ resource "aws_lambda_function" "process_lambda" {
     layers = [aws_lambda_layer_version.lambda_layer.arn]
     timeout = 60
 }
+
+resource "aws_lambda_permission" "allow_s3" {
+  action         = "lambda:InvokeFunction"
+  function_name  = aws_lambda_function.process_lambda.function_name
+  principal      = "s3.amazonaws.com"
+  source_arn     = aws_s3_bucket.processed_data_bucket.arn
+  source_account = data.aws_caller_identity.current.account_id
+}
