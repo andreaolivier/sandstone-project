ce07134e4318590becc07f8645f678b682937894
andreaolivier <andrea.olivier2004@gmail.com>
2023-11-09 15:24:10 +0000

    moves terraform layer moved to lambda file

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,3 +1,9 @@
+resource "aws_lambda_layer_version" "lambda_layer" {
+    filename   = "${path.module}/../python.zip"
+    layer_name = "pandas_module_layer"
+    compatible_runtimes = ["python3.11"]
+}
+
 resource "aws_lambda_function" "ingester_lambda" {
     # filename = "${path.module}/../${var.lambda_name}.zip"
     filename = "${path.module}/../ingester.zip"
