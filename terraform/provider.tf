d1ff86389524d716f8fe0e74290bd5d701ca7b04
pablobgldo <pablobgldo@gmail.com>
2023-11-13 14:04:59 +0000

    changed folder structure

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,3 @@
 provider "aws" {
   region     = "eu-west-2"
-  # access_key = secrets.ACCESS_KEY
-  # secret_key = secrets.SECRET_KEY
 }
--- terraform/upload_data.tf
+++
@@ -1,5 +0,0 @@
-data "archive_file" "upload_lambda" {
-    type = "zip"
-    source_file = "${path.module}/../src/upload.py"
-    output_path = "${path.module}/../upload.zip"
-}
